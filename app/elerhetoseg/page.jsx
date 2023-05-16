0d07a3e7645b7783509a786375e06b7478701ff5
Mylan1173 <milanlesko1@gmail.com>
2023-05-16 20:45:10 +0200

    Homepage 1.1

---
+++ app/elerhetoseg/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Elérhetőség - Apoapsis"
+}
+
+export default function Elerhetoseg() {
+    return (
+        <div>
+            <h1>Empty</h1>
+        </div>
+    )
+}
---
+++ app/foglalas/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Foglalás - Apoapsis"
+}
+
+export default function Fogalalas() {
+    return (
+        <div>
+
+            <h1>Empty</h1>
+        </div>
+    )
+}
