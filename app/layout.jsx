c069f084359b66b7c1f6e91aaa924fd7739eb3b6
Looky1173 <60521810+Looky1173@users.noreply.github.com>
2023-05-17 18:50:29 +0000

    Fixed favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,14 +4,6 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Főoldal - Apoapsis',
-  description: 'Apoapsis Rendezvények Kft.',
-  icons: {
-    icon: '/favicon.png',
-  },
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="hu">
