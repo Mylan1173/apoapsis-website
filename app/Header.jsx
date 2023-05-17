3021032d8acf4335de30e3e00ff7de832d77da21
Mylan1173 <milanlesko1@gmail.com>
2023-05-17 21:35:33 +0200

    Próba cseresznye

--- app/Header.jsx
+++ app/Header.jsx
@@ -14,6 +14,7 @@ const marioCartFont = localFont({
 
 export default function Header({ page }) {
 
+    const pageLoaded = useState(typeof window !== "undefined")[0]
     const [windowSmall, setWindowSmall] = useState(typeof window !== "undefined" && window.innerWidth < 1000)
 
     const [navOpen, setNavOpen] = useState(false)
@@ -49,41 +50,43 @@ export default function Header({ page }) {
 
     return (
         <header className={styles.header}>
-            {windowSmall ? (
-                <>
-                    <button className={styles.headerLeft} onClick={() => openNav(true)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.menu} fill="currentColor" viewBox="0 96 960 960"><path d="M103 844v-94h754v94H103Zm0-221v-94h754v94H103Zm0-221v-95h754v95H103Z" /></svg>
-                    </button>
-                    <div className={styles.headerRight}>
-                        <span className={marioCartFont.className}>Apoapsis</span>
-                        <Image src={Logo} alt="Apoapsis Logó" width={1000} height={1000} className={styles.logo} />
-                    </div>
+            {pageLoaded &&
+                (windowSmall ? (
+                    <>
+                        <button className={styles.headerLeft} onClick={() => openNav(true)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.menu} fill="currentColor" viewBox="0 96 960 960"><path d="M103 844v-94h754v94H103Zm0-221v-94h754v94H103Zm0-221v-95h754v95H103Z" /></svg>
+                        </button>
+                        <div className={styles.headerRight}>
+                            <span className={marioCartFont.className}>Apoapsis</span>
+                            <Image src={Logo} alt="Apoapsis Logó" width={1000} height={1000} className={styles.logo} />
+                        </div>
 
-                    {navOpen && (
-                        <nav className={styles.nav} style={{ transform: [`translateX(${navOpened ? "0%" : "-100%"})`] }}>
-                            <button className={styles.close} onClick={() => openNav(false)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill='currentColor' viewBox="0 96 960 960"><path d="m249 873-66-66 231-231-231-231 66-66 231 231 231-231 66 66-231 231 231 231-66 66-231-231-231 231Z" /></svg>
-                            </button>
+                        {navOpen && (
+                            <nav className={styles.nav} style={{ transform: [`translateX(${navOpened ? "0%" : "-100%"})`] }}>
+                                <button className={styles.close} onClick={() => openNav(false)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill='currentColor' viewBox="0 96 960 960"><path d="m249 873-66-66 231-231-231-231 66-66 231 231 231-231 66 66-231 231 231 231-66 66-231-231-231 231Z" /></svg>
+                                </button>
 
+                                <Link href={"/"} className={page === "home" ? styles.currentPage : undefined}>Főoldal</Link>
+                                <Link href={"/foglalas"} className={page === "foglalas" ? styles.currentPage : undefined} >Foglalás</Link>
+                                <Link href={"/elerhetoseg"} className={page === "elerhetoseg" ? styles.currentPage : undefined}>Elérhetőség</Link>
+                            </nav>
+                        )}
+                    </>
+                ) : (
+                    <>
+                        <div className={styles.headerLeft}>
+                            <Image src={Logo} alt="Apoapsis Logó" width={1000} height={1000} className={styles.logo} />
+                            <span className={marioCartFont.className}>Apoapsis</span>
+                        </div>
+                        <div className={styles.headerRight}>
                             <Link href={"/"} className={page === "home" ? styles.currentPage : undefined}>Főoldal</Link>
-                            <Link href={"/foglalas"} className={page === "foglalas" ? styles.currentPage : undefined} >Foglalás</Link>
+                            <Link href={"/foglalas"} className={page === "foglalas" ? styles.currentPage : undefined}>Foglalás</Link>
                             <Link href={"/elerhetoseg"} className={page === "elerhetoseg" ? styles.currentPage : undefined}>Elérhetőség</Link>
-                        </nav>
-                    )}
-                </>
-            ) : (
-                <>
-                    <div className={styles.headerLeft}>
-                        <Image src={Logo} alt="Apoapsis Logó" width={1000} height={1000} className={styles.logo} />
-                        <span className={marioCartFont.className}>Apoapsis</span>
-                    </div>
-                    <div className={styles.headerRight}>
-                        <Link href={"/"} className={page === "home" ? styles.currentPage : undefined}>Főoldal</Link>
-                        <Link href={"/foglalas"} className={page === "foglalas" ? styles.currentPage : undefined}>Foglalás</Link>
-                        <Link href={"/elerhetoseg"} className={page === "elerhetoseg" ? styles.currentPage : undefined}>Elérhetőség</Link>
-                    </div>
-                </>
-            )
+                        </div>
+                    </>
+                )
+                )
             }
 
         </header >
